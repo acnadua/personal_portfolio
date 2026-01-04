d3174d80f43be6b5296de35386945af47c3ca007
acnadua <abinadua07@gmail.com>
2026-01-04 17:26:06 +0800

    fix: removed one project (unfinished)

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,15 +1,15 @@
 import type { Project } from "../types/Project";
 
 const projects: Project[] = [
-  {
-    title: "End-to-End Reddit Data Pipeline with LLM Analysis",
-    description:
-      "Sentiment Analysis Project Redesigned: A data pipeline for collecting data from Reddit via web automation, processing data with PySpark, storing raw data in a PostgreSQL database, and then using the data for sentiment analysis.",
-    image: "/projects/reddit.png",
-    tags: ["Python", "PySpark", "PostgreSQL", "LLM", "Web Scraping", "Playwright"],
-    url: "#reddit",
-    githubUrl: "https://github.com/acnadua/Reddit-Data-Sentiment-Analysis",
-  },
+  // {
+  //   title: "End-to-End Reddit Data Pipeline with LLM Analysis",
+  //   description:
+  //     "Sentiment Analysis Project Redesigned: A data pipeline for collecting data from Reddit via web automation, processing data with PySpark, storing raw data in a PostgreSQL database, and then using the data for sentiment analysis.",
+  //   image: "/projects/reddit.png",
+  //   tags: ["Python", "PySpark", "PostgreSQL", "LLM", "Web Scraping", "Playwright"],
+  //   url: "#reddit",
+  //   githubUrl: "https://github.com/acnadua/Reddit-Data-Sentiment-Analysis",
+  // },
   {
     title: "NASA Near-Earth Object (NEO) ETL Pipeline",
     description:
